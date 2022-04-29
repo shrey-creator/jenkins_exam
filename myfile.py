6581a1cb730f9c9dcfef99843d7d43cb60aeb3f5
shrey-creator <60284082+shrey-creator@users.noreply.github.com>
2022-04-29 09:38:29 +0530

    files created

---
+++ myfile.py
@@ -0,0 +1,11 @@
+
+print("CSE306P Undergraduate Research in Computer Science Core Courses 1")
+print("ECSE302L HIGH PERFORMANCE COMPUTING Core Courses 4")
+print("ESEA301L ENVIRONMENT AND SUSTAINABILITY Core Courses 3")
+print("ECSE398J Capstone Design Core Courses 1")
+print("ECSE321P SEMINAR ON SPECIAL TOPICS IN EMERGING AREAS Core Courses 1")
+print("CSET483 Career Skills for IT Companies Open Electives 3")
+print("ECSE460L DevOps Engineering Practice Programme Electives 3")
+print("ECSE359L Software Project Management Programme Electives 3")
+print("CSET308 Communication Skills for Global Professionals Programme Electives 3")
+
